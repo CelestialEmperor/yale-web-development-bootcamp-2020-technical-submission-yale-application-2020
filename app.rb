5a7858c63a41b4e4c781ce21648bc868ac906af7
Peiyao Ma <mapeiyao@gmail.com>
2020-04-24 20:47:11 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,7 +6,10 @@ AVERAGE_HEIGHT_IN_INCHES = 67
 name = "Jack Ma"
 age = "20"
 favorite_food = "sea urchin ceviche"
-height_in_inches = "69"
+height_in_inches = 69
+
+comparison_to_average_height = height_in_inches = > 67
+
 height_message = height_in_inches ? "You are taller than average." : "You are not taller than average."
 
 ## Do not modify below this line!
