a038cc99e092a3835f44d61016188abb06af71e4
Peiyao Ma <mapeiyao@gmail.com>
2020-04-24 20:43:58 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,8 +4,8 @@ AVERAGE_HEIGHT_IN_INCHES = 67
 # Your Code Here!
 
 name = "Jack Ma"
-             age = "20"
-   favorite_food = "sea urchin ceviche"
+age = "20"
+favorite_food = "sea urchin ceviche"
 height_in_inches = "69"
 height_message 
 
