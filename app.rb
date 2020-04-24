baa329018bb5702803d50d46c7f4b9fff2424071
Peiyao Ma <mapeiyao@gmail.com>
2020-04-24 20:45:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ name = "Jack Ma"
 age = "20"
 favorite_food = "sea urchin ceviche"
 height_in_inches = "69"
-height_message = height_in_inches ? "You 
+height_message = height_in_inches ? "You are taller than average." : "You 
 
 ## Do not modify below this line!
 
